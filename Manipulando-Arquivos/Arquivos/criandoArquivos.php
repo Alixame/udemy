afc5aa60325c64ae5e4862755c290e6500cd169e
Lucas Alixame <lucasali2003@gmail.com>
2021-08-18 12:20:34 -0300

    Adicionando Material

--- Manipulando-Arquivos/Arquivos/criandoArquivos.php
+++ Manipulando-Arquivos/Arquivos/criandoArquivos.php
@@ -3,12 +3,14 @@
 date_default_timezone_set('America/Sao_Paulo');
 $nome = 'Lucas Alixame';
 $idade = 18;
+$logradoro = 'Rua Laurindo José Basso , 08 Fundos';
 
 $file = fopen("log.txt", "w+");
 
 fwrite($file, date("Y/m/d H:i:s") . "\r\n");
 fwrite($file, 'Nome: ' .$nome. "\r\n");
 fwrite($file, 'Idade: ' .$idade. "\r\n");
+fwrite($file, 'Logradoro'.$logradoro."\r\n");
 
 
 fclose($file);
