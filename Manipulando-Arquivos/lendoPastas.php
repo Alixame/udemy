ac8ef09f9e81db0caa939e862de1b0cbd116e46e
Lucas Alixame <lucasali2003@gmail.com>
2021-08-18 11:39:08 -0300

    Adicionando Material

--- Manipulando-Arquivos/lendoPastas.php
+++ Manipulando-Arquivos/lendoPastas.php
@@ -1,3 +1,29 @@
 <?php
 
 $images = scandir('images'); 
+
+$data = array();
+
+date_default_timezone_set('America/Sao_Paulo');
+
+foreach ($images as $image) {
+    
+    if (!in_array($image, array(".",".."))) {
+        
+        $filename = "images". DIRECTORY_SEPARATOR . $image;
+
+        $info = pathinfo($filename);
+        
+        $info["size"] = filesize($filename);
+        $info["modified"] = date("d/m/Y H:i:s", filemtime($filename));
+        $info["url"] = "http://localhost/cursos/udemy/Manipulando-Arquivos/".str_replace("\\","/",$filename);
+
+        array_push($data,$info);
+
+    }
+
+}
+
+echo '<pre>';
+echo json_encode($data);
+echo '</pre>';
